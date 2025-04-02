f337a42b72469a04b3374d820c2ba6cd3673efb6
Cazacu Narcis <64657192+narcisalexandru@users.noreply.github.com>
2025-04-02 14:46:22 +0300

    install primevue & phosphorIcons

---
+++ frontend/nuxt.config.ts
@@ -0,0 +1,46 @@
+import Aura from "@primeuix/themes/aura";
+
+export default defineNuxtConfig({
+  modules: ["@primevue/nuxt-module"],
+
+  app: {
+    head: {
+      title: "Maini in Lut",
+      meta: [
+        { charset: "utf-8" },
+        { name: "viewport", content: "width=device-width, initial-scale=1" },
+        { name: "description", content: "Maini in LUT - Website" },
+      ],
+      link: [
+        { rel: "icon", type: "image/x-icon", href: "/favicon.ico" },
+        {
+          rel: "stylesheet",
+          type: "text/css",
+          href: "https://cdn.jsdelivr.net/npm/@phosphor-icons/web@2.1.2/src/regular/style.css",
+        },
+        {
+          rel: "stylesheet",
+          type: "text/css",
+          href: "https://cdn.jsdelivr.net/npm/@phosphor-icons/web@2.1.1/src/fill/style.css",
+        },
+      ],
+    },
+  },
+
+  primevue: {
+    options: {
+      ripple: true,
+      inputVariant: "filled",
+      theme: {
+        preset: Aura,
+        options: {
+          prefix: "p",
+          darkModeSelector: "system",
+          cssLayer: false,
+        },
+      },
+    },
+  },
+
+  compatibilityDate: "2025-04-02",
+});
