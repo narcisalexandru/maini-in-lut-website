d8d023c96ad71e44216411cecd71d04ab13a3f16
Cazacu Narcis <64657192+narcisalexandru@users.noreply.github.com>
2025-06-23 13:29:18 +0300

    fix: now darkMode is disabled

--- frontend/nuxt.config.ts
+++ frontend/nuxt.config.ts
@@ -56,7 +56,7 @@ export default defineNuxtConfig({
         preset: Aura,
         options: {
           prefix: "p",
-          darkModeSelector: "system",
+          darkModeSelector: false,
           cssLayer: false,
         },
       },
